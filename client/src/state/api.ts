a38ab7cb1440d8a49286c49fd92c2bd122ccc18c
ell-91 <cdsherman@ucdavis.edu>
2024-05-09 16:03:26 -0400

    webpage layout, theme and API setup

---
+++ client/src/state/api.ts
@@ -0,0 +1,18 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+// Alows us to make endpoints that we can use to call our backend 
+//VITE_BASE_URL is the enviornment variable that we created - the url we call everytime we make an api call 
+export const api = createApi({
+    baseQuery: fetchBaseQuery({ baseUrl: import.meta.env.VITE_BASE_URL }),
+    reducerPath: "main",
+    tagTypes: ["Kpis", "Products", "Transactions"],
+    // API endpoints
+    endpoints: (build) => ({
+        getKpis: build.query<void,void>({
+          query: () => "kpi/kpis/",
+          providesTags: ["Kpis"],
+        }),
+    })
+})
+
+export const { useGetKpisQuery} = api;
