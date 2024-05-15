d88c708c0c18a48ca7cd5b218fbf9d3f65f2fd33
ell-91 <cdsherman@ucdavis.edu>
2024-05-14 21:47:12 -0400

    Area chart implementation

--- client/src/state/api.ts
+++ client/src/state/api.ts
@@ -1,4 +1,5 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { GetKpisResponse } from "./types";
 
 // Alows us to make endpoints that we can use to call our backend 
 //VITE_BASE_URL is the enviornment variable that we created - the url we call everytime we make an api call 
@@ -8,7 +9,7 @@ export const api = createApi({
     tagTypes: ["Kpis", "Products", "Transactions"],
     // API endpoints
     endpoints: (build) => ({
-        getKpis: build.query<void,void>({
+        getKpis: build.query<Array<GetKpisResponse>,void>({
           query: () => "kpi/kpis/",
           providesTags: ["Kpis"],
         }),
